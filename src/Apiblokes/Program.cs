338ee37a8b775df59bbce8286938a9a89c398e69
Mark Lee <mark@9embers.com>
2025-09-08 18:03:54 -0400

    Added the ability to move around the world grid

--- src/Apiblokes/Program.cs
+++ src/Apiblokes/Program.cs
@@ -41,4 +41,54 @@ app.MapPost( "/", () =>
     return player.Id;
 } );
 
+
+app.MapPost( "/move/{direction}", ( HttpContext context, string direction ) =>
+{
+    var auth = context.Request.Headers.Authorization.FirstOrDefault();
+
+    if ( string.IsNullOrEmpty( auth ) )
+    {
+        context.Response.StatusCode = 400;
+        return "Please post to this URL to get a player Id. Then put the Id in the Authorization header";
+    }
+
+    if ( !Guid.TryParse( auth.Replace( "Bearer ", "" ), out var id ) )
+    {
+        return $"{auth} is not a valid header";
+    }
+
+    var dataContext = new DataContext();
+    var player = dataContext.Players.FirstOrDefault( p => p.Id == id );
+
+    if ( player == null )
+    {
+        context.Response.StatusCode = 404;
+        return "Player not found";
+    }
+
+    var simpleDir = direction.ToLower().FirstOrDefault();
+
+    switch ( simpleDir )
+    {
+        case 'n':
+            player.Y += 1;
+            break;
+        case 's':
+            player.Y -= 1;
+            break;
+        case 'w':
+            player.X -= 1;
+            break;
+        case 'e':
+            player.Y += 1;
+            break;
+        default:
+            break;
+    }
+
+    dataContext.SaveChanges();
+
+    return $"You are in a vast field. You see wild Apiblokes scattering before you. Location: {player.X}:{player.Y}";
+} );
+
 app.Run();
